f96a8a2080501dffd96163548a883352aaf4822c
Anly-S <anlysolly@gmail.com>
2024-04-01 11:48:09 +0530

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,25 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'AngularApp';
+  public msg = 'Hello';
+  onchange(val: any) {
+    console.log(val);
+    this.msg = val;
+  }
+
+  public fruits = [
+    { name: 'Apple', price: 12 },
+    { name: 'Orange', price: 4 },
+    { name: 'Kiwi', price: 2 },
+  ];
 }
