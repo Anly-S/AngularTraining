d10997f8e6c4557e281e7d0b8436a4a787a6e4a8
Anly-S <anlysolly@gmail.com>
2024-04-01 14:39:46 +0530

    feat:form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,30 +2,23 @@ import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, CommonModule, HttpClientModule],
+  imports: [RouterOutlet, CommonModule, HttpClientModule, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'AngularApp';
   public msg = '';
-  public data: any;
-
-  constructor(private http: HttpClient) {}
-  ngOnInit() {
-    this.fetchDetails();
-  }
-
-  fetchDetails() {
-    this.http
-      .get<any>('https://jsonplaceholder.typicode.com/posts')
-      .subscribe((resp: any) => {
-        console.log(resp);
-        this.data = resp;
-      });
+  user: any = {
+    firstName: '',
+    lastName: '',
+  };
+  onSubmit() {
+    console.log(this.user);
   }
 }
